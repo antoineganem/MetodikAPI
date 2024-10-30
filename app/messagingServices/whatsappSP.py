2c29043744c020006e3acf78c1f65a6fc90f75f9
[antoine ganem] <[a01644024@tec.mx]>
2024-10-30 16:09:19 -0600

    QR code

--- app/messagingServices/whatsappSP.py
+++ app/messagingServices/whatsappSP.py
@@ -3,93 +3,42 @@ from flask import jsonify
 import os 
 from dotenv import load_dotenv
 from app.utils.db import get_db_connection, close_db_connection
+from app.utils.db_helpers import execute_stored_procedure
 
 #Store procedure to obtain the data to send the message 
 def message_data(data):
-    conn = None
-    try: 
-        conn = get_db_connection()
-        cursor = conn.cursor()
-        query = "EXEC spMandarWhatsapp ?,?,?"
-        cursor.execute(query,
-                       data.get("ID"),
-                       data.get("RenglonID"),
-                       data.get("HorarioRutaID"))
+
+    sp_name = "spMandarWhatsapp"
+    params = [
+        data.get("ID"),
+        data.get("RenglonID"),
+        data.get("HorarioRutaID")]
         
-        while cursor.description is None:
-            cursor.nextset()
-        if cursor.description is None:
-            return jsonify({"error": "No data returned from the procedure."}), 500
-        
-        columns = [column[0] for column in cursor.description]
+    return execute_stored_procedure(sp_name, params)
 
-        results = [dict(zip(columns, row)) for row in cursor.fetchall()]
-
-        return jsonify(results, 200)
-    except pyodbc.Error as e:
-        return ({"error": str(e)}), 500
-    finally:
-        if conn:
-            close_db_connection(conn)
-
-def leerMensajesPorWAID(data):
-    conn = None
-    try: 
-        conn = get_db_connection()
-        cursor = conn.cursor()
-        query = "EXEC spLeerMensajesPorWAID ?"
-        cursor.execute(query,
-                       data.get("wa_id"))
-        
-        while cursor.description is None:
-            cursor.nextset()
-
-        if cursor.description is None:
-            return jsonify({"error": "No data returned from the procedure."}), 500
-        
-        columns = [column[0] for column in cursor.description]
-
-        results = [dict(zip(columns, row)) for row in cursor.fetchall()]
-
-        return jsonify(results, 200)
+def leerMensajesPorWAID(data): 
     
-    except pyodbc.Error as e:
-        return ({"error": str(e)}), 500
-    
-    finally:
-        if conn:
-            close_db_connection(conn)
+    sp_name = "spLeerMensajesPorWAID"
+    params = [
+        data.get("wa_id")]
+
+    return execute_stored_procedure(sp_name, params)
 
 def actMensajeWAPP(data):
-    conn = None 
-    try: 
-        conn = get_db_connection()
-        cursor = conn.cursor()
-        query = "EXEC spActMensajesWAPP ?,?,?,?,?"
-        print(data.get("wa_id"),data.get("nombre"),data.get("mensaje"),data.get("remitente"),data.get("tipoMensaje"))
-        cursor.execute(query,
-                       data.get("wa_id"),
-                       data.get("nombre"),
-                       data.get("mensaje"),
-                       data.get("remitente"),
-                       data.get("tipoMensaje"))
-
-        while cursor.description is None:
-            cursor.nextset()
-
-        if cursor.description is None:
-            return jsonify({"error": "No data returned from the procedure."}), 500
-        
-        columns = [column[0] for column in cursor.description]
-
-        results = [dict(zip(columns, row)) for row in cursor.fetchall()]
-
-        return (results),200
     
-    except pyodbc.Error as e:
-        print(e)
-        return ({"error": str(e)}), 500
-    
-    finally:
-        if conn:
-            close_db_connection(conn)
+    sp_name = "spActMensajesWAPP"
+    params = [
+        data.get("wa_id"),
+        data.get("nombre"),
+        data.get("mensaje"),
+        data.get("remitente"),
+        data.get("tipoMensaje")]
+
+    return execute_stored_procedure(sp_name, params) 
+       
+def verUsuariosWAPP():
+
+    sp_name = "spVerUsuariosWAPP"
+    params = []
+
+    return execute_stored_procedure(sp_name, params)
--- app/routes/Whatsapp/whatsapp.py
+++ app/routes/Whatsapp/whatsapp.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request
-from app.services.whatsappAPI.whatsapp_services import send_template_service, get_message_data, send_message_service, verify_service, handle_message_service
+from app.services.whatsappAPI.whatsapp_services import send_template_service, get_message_data, send_message_service, verify_service, handle_message_service, upload_media_service, start_conversation, leerMensajesPorWAID_service, verUsuariosWAPP_service
+from app.services.whatsappAPI.whatsapp_services import leerMensajesPorWAID_service, verUsuariosWAPP_service
 from flask_jwt_extended import  jwt_required
 
 whatsapp_bp = Blueprint('whatsapp', __name__)
@@ -7,6 +8,10 @@ get_message_data_bp = Blueprint('getMessageData', __name__)
 send_message_bp = Blueprint('sendMessageData', __name__)
 webhook_verify_bp = Blueprint('webhook', __name__)
 webhook_bp = Blueprint('webhookRoute', __name__)
+upload_media_bp = Blueprint('uploadMedia', __name__)
+start_conversation_bp = Blueprint('startConversation', __name__)
+leerMensajesPorWAID_bp = Blueprint('leerMensajesPorWAID', __name__)
+verUsuariosWAPP_bp = Blueprint('verUsuariosWAPP', __name__)
 
 @whatsapp_bp.route('/send_message', methods=['POST'])
 def send_message_route():
@@ -19,7 +24,7 @@ def send_message_route():
 def get_message_data_route():
     data = request.json
     get_message_data_response = get_message_data(data)
-    return get_message_data_response, 200
+    return get_message_data_response 
 
 @send_message_bp.route('/answer_message', methods=['POST'])
 @jwt_required()
@@ -28,7 +33,7 @@ def answer_message_route():
     recipient_WAID = data.get('recipient_WAID')
     text = data.get('text')
     send_message_response = send_message_service(recipient_WAID, text)
-    return send_message_response, 200
+    return send_message_response 
 
 # Webhook verification route
 @webhook_verify_bp.route('/webhook', methods=['GET'])
@@ -39,3 +44,31 @@ def webhook_route():
 @webhook_bp.route('/webhook', methods=['POST'])
 def webhook_route():
     return handle_message_service()
+
+@upload_media_bp.route('/upload_media', methods=['POST'])
+@jwt_required()
+def upload_media_route():
+    data = request.json
+    upload_media_response = upload_media_service(data)
+    return upload_media_response 
+
+@start_conversation_bp.route('/start_conversation', methods=['POST'])
+@jwt_required()
+def start_conversation_route():
+    data = request.json
+    start_conversation_response = start_conversation(data)
+    return start_conversation_response 
+
+@leerMensajesPorWAID_bp.route('/leerMensajes', methods=['POST'])
+@jwt_required()
+def leerMensajesPorWAID_route():
+    data = request.json
+    print(data)
+    leerMensajesPorWAID_response = leerMensajesPorWAID_service(data)
+    return leerMensajesPorWAID_response
+
+@verUsuariosWAPP_bp.route('/verUsuarios', methods=['GET'])
+@jwt_required()
+def verUsuariosWAPP_route():
+    verUsuariosWAPP_response = verUsuariosWAPP_service()
+    return verUsuariosWAPP_response
