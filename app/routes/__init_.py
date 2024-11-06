d04b55f4c7defddfa4cdd5042b285afa3271417d
Emilio Ortiz <su1uv@luvly.local>
2024-11-05 20:53:49 -0600

    Catalogo Choferes

--- app/routes/__init_.py
+++ app/routes/__init_.py
@@ -16,7 +16,7 @@ from app.routes.Comercial.Reservas.ReservasD import *
 from app.routes.Comercial.Paqueteria.Paqueteria import *
 from app.routes.Exploradores.RutasExplorador import verExploradorRutas_bp, verExploradorRutasID_bp
 from app.routes.Catalogos.Rutas import rutas_bp, rutasResumen_bp, actRuta_bp, verHorarios_bp, actHorarioRuta_bp, verRutasHorarios_bp, eliminarRutaHorario_bp
-from app.routes.Catalogos.Choferes import choferes_bp, choferesResumen_bp, actChoferes_bp, verChoferID_bp
+from app.routes.Catalogos.Choferes import verChoferes_bp, verChoferID_bp, actChoferD_bp, eliminarChofer_bp
 from app.routes.Catalogos.Agentes import agentes_bp, agentesResumen_bp, actAgente_bp, verAgenteID_bp
 from app.routes.Whatsapp.whatsapp import whatsapp_bp, get_message_data_bp
 from app.routes.Logistica.Rutas.Rutas import *
@@ -157,10 +157,10 @@ def register_routes(app: Flask):
     app.register_blueprint(eliminarRutaHorario_bp)
 
     #Catalogo de Choferes
-    app.register_blueprint(choferes_bp)
-    app.register_blueprint(choferesResumen_bp)
-    app.register_blueprint(actChoferes_bp)
+    app.register_blueprint(verChoferes_bp)
     app.register_blueprint(verChoferID_bp)
+    app.register_blueprint(actChoferD_bp)
+    app.register_blueprint(eliminarChofer_bp)
     
     #Whatsapp api 
     app.register_blueprint(whatsapp_bp)
