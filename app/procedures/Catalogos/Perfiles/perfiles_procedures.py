a2f2028d4160047f2f89539c86eec94e1c2d822a
FivesLuis <agomez@fivesconsulting.com>
2024-11-08 15:13:55 -0600

    adecuacion de perfiles

--- app/procedures/Catalogos/Perfiles/perfiles_procedures.py
+++ app/procedures/Catalogos/Perfiles/perfiles_procedures.py
@@ -1,116 +1,27 @@
-import pyodbc
-from flask import jsonify
-from app.utils.db import get_db_connection, close_db_connection
-import logging
+from app.utils.db_helpers import execute_stored_procedure
 
 def ver_Perfiles(EstatusID, EmpresaID, Buscar):
-    conn = None
-    try:
-        conn = get_db_connection()
-        cursor = conn.cursor()
-        conn.autocommit = True  
-
-        query = "EXEC spVerPerfiles ?,?,?"
-        cursor.execute(query, EstatusID, EmpresaID, Buscar)
-        
-
-        while cursor.description is None:
-            cursor.nextset()
-
-        if cursor.description is None:
-            return {"error": "No data returned from the procedure."}, 500
-
-        columns = [column[0] for column in cursor.description]
-        results = [dict(zip(columns, row)) for row in cursor.fetchall()]
-        return results, 200  
-    except pyodbc.Error as e:
-        if conn:
-            conn.rollback()  # En caso de error, revertir la transacción
-        return {"error": str(e)}, 500  
-    finally:
-        if conn:
-            close_db_connection(conn)
+    sp_name = "spVerPerfiles"
+    params = [ EstatusID, EmpresaID, Buscar ]
+    return execute_stored_procedure(sp_name, params)
             
 def ver_PerfilID(ID):
-    conn = None
-    try:
-        conn = get_db_connection()
-        cursor = conn.cursor()
-        conn.autocommit = True  
-
-        query = "EXEC spVerPerfilID ?"
-        cursor.execute(query, ID)
-        
-
-        while cursor.description is None:
-            cursor.nextset()
-
-        if cursor.description is None:
-            return {"error": "No data returned from the procedure."}, 500
-
-        columns = [column[0] for column in cursor.description]
-        results = [dict(zip(columns, row)) for row in cursor.fetchall()]
-        return results, 200  
-    except pyodbc.Error as e:
-        if conn:
-            conn.rollback()  # En caso de error, revertir la transacción
-        return {"error": str(e)}, 500  
-    finally:
-        if conn:
-            close_db_connection(conn)
-            
+    sp_name = "spVerPerfilID"
+    params = [ ID ]
+    return execute_stored_procedure(sp_name, params)
 
 def act_Perfil(data):
-    conn = None
-    try:
-        conn = get_db_connection()
-        cursor = conn.cursor()
-        conn.autocommit = True  # Asegúrate de que las transacciones se confirmen automáticamente
-
-
-        query = "EXEC spActPerfiles ?,?,?,?,?"
-        cursor.execute(query, data.get("ID"), data.get("Nombre"), data.get("Notas"), data.get("EstatusID"), data.get("EmpresaID"))
-
-        
-        while cursor.description is None:
-            cursor.nextset()
-
-        if cursor.description is None:
-            return {"error": "No data returned from the procedure."}, 500
-
-        columns = [column[0] for column in cursor.description]
-        results = [dict(zip(columns, row)) for row in cursor.fetchall()]
-        return results, 200  
-    except pyodbc.Error as e:
-        return {"error": str(e)}, 500  
-    finally:
-        if conn:
-            close_db_connection(conn)
+    sp_name = "spActPerfiles"
+    params = [
+        data.get("ID"), 
+        data.get("Nombre"), 
+        data.get("Notas"), 
+        data.get("EstatusID"), 
+        data.get("EmpresaID"),
+    ]
+    return execute_stored_procedure(sp_name, params)
 
 def ver_ModulosAcceso(PerfilID, PersonaID):
-    conn = None
-    try:
-        conn = get_db_connection()
-        cursor = conn.cursor()
-        conn.autocommit = True  
-
-        query = "EXEC spVerModulosAcceso ?,?"
-        cursor.execute(query, PerfilID, PersonaID)
-        
-
-        while cursor.description is None:
-            cursor.nextset()
-
-        if cursor.description is None:
-            return {"error": "No data returned from the procedure."}, 500
-
-        columns = [column[0] for column in cursor.description]
-        results = [dict(zip(columns, row)) for row in cursor.fetchall()]
-        return results, 200  
-    except pyodbc.Error as e:
-        if conn:
-            conn.rollback()  # En caso de error, revertir la transacción
-        return {"error": str(e)}, 500  
-    finally:
-        if conn:
-            close_db_connection(conn)
+    sp_name = "spVerModulosAcceso"
+    params = [ PerfilID, PersonaID ]
+    return execute_stored_procedure(sp_name, params)
